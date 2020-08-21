9a329acd4596080a076c6c4ad819732aa411c213
MikeWu597 <34954512+MikeWu597@users.noreply.github.com>
2020-08-21 10:39:15 +0800

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,25 +1,25 @@
 <?php
-$ctrlip="45.11.1.197";   //被控端IP
-$ctrlpw="0N9UfzMf1ot6C7Nu8P";   //被控端密码
-$ctrlport=22; //被控端端口
-$ctrlusr="root";  //被控端用户名（使用Linux）
-$ip=$_POST["ipname"].".hyp.ink"; //域名后缀默认hyp.ink，此处请修改
+$aki='';     //Access Key ID，请修改
+$aks='';   //Access Key Secret，请修改
+$ip=$_POST["ipname"].".hyp.ink";       //域名后缀默认hyp.ink，此处请修改
+$sgid='';       //要控制的安全组的ID，请修改
+
 $client=$_SERVER["REMOTE_ADDR"];
+
 echo "你所请求的IP：".$ip;
 echo "<br>";
 echo "欲绑定的用户IP：".$client;
 echo "<br>绑定结果：<br>";
 $ret=file_get_contents("https://mcapi.us/server/status?ip=".$ip);
 $dec=json_decode($ret);
+
 if($dec->status=="success")
 {
-	echo "当前请求的IP可用";
-	$connection=ssh2_connect($ctrlip,$ctrlport);
-	ssh2_auth_password($connection,$ctrlusr,$ctrlpw);
-//	$cmd="mkdir /home/remotesucc";
-//	$return_d=ssh2_exec($connection,$cmd);
-	stream_set_blocking($return_d,true);
-	echo $return_d;
+	echo "成功！已经放行你的IP";
+	$cmd='aliyun ecs AuthorizeSecurityGroup --mode AK --access-key-id ' . $aki . ' --access-key-secret ' . $aks .' --region cn-shanghai --RegionId cn-shanghai --SecurityGroupId ' . $sgid . ' --IpProtocol tcp --PortRange 25565/25565 --SourceCidrIp ' . $client . ' --Policy accept --Priority 1';
+//	echo $cmd;
+	shell_exec($cmd);
+	
 }
 else
 {
